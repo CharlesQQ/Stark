6cc23a927cdce4f69d1802b7e314a61e79a7e7f0
charles <charles_ali@qq.com>
2016-12-26 00:22:04 +0800

    创建表

--- Arya/models.py
+++ Arya/models.py
@@ -4,4 +4,19 @@ from django.db import models
 
 class Host(models.Model):
     hostname=models.CharField(max_length=123,unique=True)
+    key = models.TextField()
+    status_choices=((0,'Waiting Approval'),
+                    (1,'Accepted'),
+                    (2,'Rejected'))
 
+    status=models.SmallIntegerField(choices=status_choices,default=0)
+    
+    def __str__(self):
+        return self.hostname
+
+class HostGroup(models.Model):
+    name = models.CharField(max_length=64,unique=64)
+    hosts=models.ManyToManyField(Host,blank=True)
+
+    def __str__(self):
+        return self.name
