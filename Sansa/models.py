8ea711c3ce81a6d23f22c52d83defa95cc35cce1
charles <charles_ali@qq.com>
2017-01-12 00:28:51 +0800

    自定义用户认证和admin

--- Sansa/models.py
+++ Sansa/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from Wolf.models import UserProfile
 
 class Asset(models.Model):
-
     asset_type_choices = (
         ('server', u'服务器'),
         ('networkdevice', u'网络设备'),
@@ -35,7 +34,7 @@ class Asset(models.Model):
     price = models.FloatField(u'价格',null=True, blank=True)
     business_unit = models.ForeignKey('BusinessUnit', verbose_name=u'所属业务线',null=True, blank=True)
     tags = models.ManyToManyField('Tag' ,blank=True)
-    admin = models.ForeignKey('UserProfile', verbose_name=u'资产管理员',null=True, blank=True)
+    admin = models.ForeignKey(UserProfile, verbose_name=u'资产管理员',null=True, blank=True)
     idc = models.ForeignKey('IDC', verbose_name=u'IDC机房',null=True, blank=True)
 
     status_choices = ((0,'在线'),
@@ -313,7 +312,7 @@ class IDC(models.Model):
 
 class Tag(models.Model):
     name = models.CharField('Tag name',max_length=32,unique=True )
-    creater = models.ForeignKey('UserProfile')
+    creater = models.ForeignKey(UserProfile)
     create_date = models.DateField(auto_now_add=True)
     def __str__(self):
         return self.name
@@ -334,7 +333,7 @@ class EventLog(models.Model):
     component = models.CharField('事件子项',max_length=255, blank=True,null=True)
     detail = models.TextField(u'事件详情')
     date = models.DateTimeField(u'事件时间',auto_now_add=True)
-    user = models.ForeignKey('UserProfile',verbose_name=u'事件源')
+    user = models.ForeignKey(UserProfile,verbose_name=u'事件源')
     memo = models.TextField(u'备注', blank=True,null=True)
 
     def __str__(self):
@@ -382,7 +381,7 @@ class NewAssetApprovalZone(models.Model):
     data = models.TextField(u'资产数据')
     date = models.DateTimeField(u'汇报日期',auto_now_add=True)
     approved = models.BooleanField(u'已批准',default=False)
-    approved_by = models.ForeignKey('UserProfile',verbose_name=u'批准人',blank=True,null=True)
+    approved_by = models.ForeignKey(UserProfile,verbose_name=u'批准人',blank=True,null=True)
     approved_date = models.DateTimeField(u'批准日期',blank=True,null=True)
 
     def __str__(self):
--- Wolf/models.py
+++ Wolf/models.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import (
 )
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, password=None):
+    def create_user(self, email, name, password=None):
         """
         Creates and saves a User with the given email, date of
         birth and password.
@@ -22,14 +22,14 @@ class MyUserManager(BaseUserManager):
 
         user = self.model(
             email=self.normalize_email(email),
-            date_of_birth=date_of_birth,
+            name = name,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, date_of_birth, password):
+    def create_superuser(self, email, name, password):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
@@ -37,7 +37,7 @@ class MyUserManager(BaseUserManager):
         user = self.create_user(
             email,
             password=password,
-            date_of_birth=date_of_birth,
+            name=name,
         )
         user.is_admin = True
         user.save(using=self._db)
